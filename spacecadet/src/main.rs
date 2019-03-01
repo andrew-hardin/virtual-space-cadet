d2be4e489783f7ab64dc8197bc01d0c3dae86610
Andrew Hardin <andrew.hardin5+git@gmail.com>
2019-02-28 22:17:31 -0700

    Create a simple 1D keyboard layout that reflects current development board.

--- spacecadet/src/main.rs
+++ spacecadet/src/main.rs
@@ -3,10 +3,11 @@ use libspacecadet::*;
 
 fn get_keypad_matrix() -> KeyMatrix {
     return vec![
-        vec![Some(KEY::KEY_NUMLOCK), Some(KEY::KEY_KPSLASH), Some(KEY::KEY_KPASTERISK)],
-        vec![Some(KEY::KEY_KP7), Some(KEY::KEY_KP8), Some(KEY::KEY_KP9)],
-        vec![Some(KEY::KEY_KP4), Some(KEY::KEY_KP5), Some(KEY::KEY_KP6)],
-        vec![Some(KEY::KEY_KP1), Some(KEY::KEY_KP2), Some(KEY::KEY_KP3)]
+        vec![Some(KEY::KEY_ESC)],
+        vec![Some(KEY::KEY_TAB)],
+        vec![Some(KEY::KEY_CAPSLOCK)],
+        vec![Some(KEY::KEY_LEFTSHIFT)],
+        vec![Some(KEY::KEY_RIGHTSHIFT)],
     ]
 }
 
@@ -15,20 +16,17 @@ fn base_layer_keys() -> KeyCodeMatrix {
         key: Box::new(KEY::KEY_9),
         modifier: KEY::KEY_LEFTSHIFT
     });
-
-    let mut ans = KeyCodeMatrix::new((4,3));
-    ans.codes[0][0] = Box::new(OneShotLayer {
-        layer_name: "second".to_string()
+    let right_paren = Box::new(ModifierWrappedKey {
+        key: Box::new(KEY::KEY_9),
+        modifier: KEY::KEY_RIGHTSHIFT
     });
-    ans.codes[3][0] = Box::new(SpaceCadet::new(left_paren, KEY::KEY_LEFTSHIFT));
-    ans.codes[3][1] = Box::new(KEY::KEY_C);
-    ans
-}
 
-fn secondary_layer_keys() -> KeyCodeMatrix {
-    let ans = KeyCodeMatrix::new((4, 3));
-    //ans.codes[0][0] = Box::new(KEY::KEY_9);
-    //ans.codes[3][0] = Box::new(KEY::KEY_2);
+    let mut ans = KeyCodeMatrix::new((5, 1));
+    ans.codes[1][0] = Box::new(KEY::KEY_CAPSLOCK);
+    ans.codes[1][0] = Box::new(KEY::KEY_ESC);
+    ans.codes[2][0] = Box::new(KEY::KEY_TAB);
+    ans.codes[3][0] = Box::new(SpaceCadet::new(left_paren, KEY::KEY_LEFTSHIFT));
+    ans.codes[4][0] = Box::new(SpaceCadet::new(right_paren, KEY::KEY_RIGHTSHIFT));
     ans
 }
 
@@ -69,7 +67,6 @@ fn main() {
     };
 
     f.add_layer(LayerAttributes { name: "base".to_string(), enabled: true }, base_layer_keys());
-    f.add_layer(LayerAttributes { name: "second".to_string(), enabled: false }, secondary_layer_keys());
 
     let mut update = || tick(&mut f);
     cyclic_executor(&mut update, 200);
