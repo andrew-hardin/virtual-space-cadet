42612ff79b9b8627d13d398c4a651c994c4bce21
Andrew Hardin <andrew.hardin5+git@gmail.com>
2019-03-04 21:59:20 -0700

    Add docstrings.

--- libspacecadet/src/virtual_keyboard_matrix.rs
+++ libspacecadet/src/virtual_keyboard_matrix.rs
@@ -7,6 +7,7 @@ use crate::keys;
 pub type KeyMatrix = Vec<Vec<Option<keys::KEY>>>;
 pub type Index2D = (usize, usize);
 
+/// A key can undergo three state changes: press, release, and hold.
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 pub enum KeyStateChange {
     Released = 0,
@@ -14,6 +15,8 @@ pub enum KeyStateChange {
     Held = 2,
 }
 
+/// Shorthand for converting KeyStateChange into libevdev/uniput
+/// compatible values.
 impl Into<KeyStateChange> for i32 {
     fn into(self) -> KeyStateChange {
         match self {
@@ -25,21 +28,26 @@ impl Into<KeyStateChange> for i32 {
     }
 }
 
+/// Plain-old-data struct that stores counts for each key state change
+/// (e.g. 32 key presses).
 #[derive(Copy, Clone)]
 pub struct KeyStats {
     values: [u32; 3]
 }
 
 impl KeyStats {
+    /// Create new key statistics initialized to zero.
     pub fn new() -> KeyStats {
         KeyStats {
             values: [0, 0, 0]
         }
     }
+    /// Increment the number of times a particular state change was seen.
     pub fn increment(&mut self, v: KeyStateChange) {
         self.values[v as usize] += 1;
     }
 
+    /// Get the number of times a state change was seen.
     pub fn get(&self, v: KeyStateChange) -> u32 {
         self.values[v as usize]
     }
@@ -51,11 +59,13 @@ pub struct BlockedKeyStates {
 }
 
 impl BlockedKeyStates {
+    /// Create a new block that doesn't block any key state changes.
     pub fn new() -> BlockedKeyStates {
         BlockedKeyStates {
             blocked: [false; 3]
         }
     }
+    /// Create a new block for key releases and holds.
     pub fn new_block_release_and_hold() -> BlockedKeyStates {
         let mut ans = BlockedKeyStates::new();
         ans.blocked[KeyStateChange::Released as usize] = true;
@@ -63,6 +73,7 @@ impl BlockedKeyStates {
         ans
     }
 
+    /// Returns `true` if a key state change is blocked.
     fn check_if_blocked(&mut self, s: KeyStateChange) -> bool {
         if s == KeyStateChange::Held && self.blocked[s as usize] {
             // Multiple holds are blocked.
@@ -77,23 +88,31 @@ impl BlockedKeyStates {
         }
     }
 
+    /// Unblock all key state changes.
     fn unblock(&mut self) {
         self.blocked = [false; 3];
     }
 }
 
+/// Return states from after updating `VirtualKeyboardMatrix`.
 #[derive(Copy, Clone)]
 pub enum MatrixUpdateResult {
-    // The given event wasn't within the matrix.
+    /// The given event wasn't within the matrix.
     Bypass,
-    // The event was redundant (e.g. pressing a pressed key).
+    /// The event was redundant (e.g. pressing a pressed key).
     Redundant(Index2D),
-    // The event caused a state to change.
+    /// The event caused a state to change at the given index.
     StateChanged(Index2D, KeyStateChange),
-    // The event caused a state change, but it was blocked/masked.
+    /// The event caused a state change, but it was blocked.
     Blocked
 }
 
+/// Transforms discrete events (e.g. `KEY_A` was pressed)
+/// into a classic NxM state matrix.
+///
+/// This is a workhorse structure that bridges the gap between
+/// physical events (e.g. the user pressing a key) and our internal
+/// representation of the keyboard state -- an MxN matrix.
 pub struct VirtualKeyboardMatrix {
     key_to_index: HashMap<evdev::enums::EV_KEY, Index2D>,
     dim: Index2D,
@@ -102,6 +121,10 @@ pub struct VirtualKeyboardMatrix {
 }
 
 impl VirtualKeyboardMatrix {
+    /// Create a new virtual keyboard matrix by specifying which keys are
+    /// at which positions in the matrix.
+    ///
+    /// For example, `KEY_A` is at row 3, column 2.
     pub fn new(keys: KeyMatrix) -> VirtualKeyboardMatrix {
 
         // Loop through the event matrix and store a map from event -> index.
@@ -127,19 +150,20 @@ impl VirtualKeyboardMatrix {
         }
     }
 
+    /// Block key events at the given index.
     pub fn set_block(&mut self, block: BlockedKeyStates, idx: Index2D) {
         self.blocked[idx.0][idx.1] = block;
     }
 
+    /// Update the matrix using an input event (e.g. `KEY_A` was pressed).
+    /// Events can be blocked (see `set_block`).
     pub fn update(&mut self, event: evdev::InputEvent) -> MatrixUpdateResult {
         // Update the matrix, then check if the event is blocked.
-        // A blocked event returns a None...
         let ans = self.update_without_blocking(event);
         self.check_blocking(ans)
     }
 
-    // Update the matrix state by processing a single event.
-    // Returns a bool indicating if the event was in the matrix.
+    /// Update the matrix state by processing a single event.
     fn update_without_blocking(&mut self, event: evdev::InputEvent) -> MatrixUpdateResult {
 
         // Convert the event time into a friendly representation.
@@ -175,6 +199,10 @@ impl VirtualKeyboardMatrix {
         }
     }
 
+    /// Detect any keys that have been held longer than the specified duration.
+    /// Holds can be blocked (see `set_block`).
+    ///
+    /// Returns a vector of positions where keys have been held.
     pub fn detect_held_keys(&mut self, held_key_threshold: Duration, now: SystemTime) -> Vec<Index2D> {
         self.detect_held_keys_without_blocking(held_key_threshold, now).iter()
             .filter(|x| {
@@ -215,12 +243,14 @@ impl VirtualKeyboardMatrix {
     }
 }
 
+/// A 2D matrix that records where and when key presses occurred.
 struct StateMatrix {
     state: Vec<Vec<bool>>,
     last_pressed: Vec<Vec<SystemTime>>,
 }
 
 impl StateMatrix {
+    /// Create a new state NxM state matrix with every key unpressed.
     pub fn new(dim: Index2D) -> StateMatrix {
         // Initialize our state with every key unpressed.
         StateMatrix {
@@ -229,6 +259,10 @@ impl StateMatrix {
         }
     }
 
+    /// Set a key's binary state (i.e. is it pressed or not).
+    ///
+    /// If the key's state changed (e.g. pressed -> released), then
+    /// Some() is returned.
     pub fn set(&mut self, dim: Index2D, is_pressed: bool, now: SystemTime) -> Option<KeyStateChange> {
         let old_state = self.state[dim.0][dim.1];
         let new_state = is_pressed;
@@ -246,12 +280,14 @@ impl StateMatrix {
         }
     }
 
+    /// Check if a key at the given index has been held longer than a specified duration.
     pub fn is_held(&self, idx: Index2D, hold_threshold: Duration, now: SystemTime) -> bool {
         let is_pressed = self.state[idx.0][idx.1];
         let held_long_enough = self.last_pressed[idx.0][idx.1] + hold_threshold <= now;
         is_pressed && held_long_enough
     }
 
+    /// Reset the timestamp for when a key was last pressed.
     pub fn reset_key_press_time(&mut self, idx: Index2D, when: SystemTime) {
         self.last_pressed[idx.0][idx.1] = when;
     }
