d44799b0291c963a8d6a58aa0f518aa2df1eba9e
Andrew Hardin <andrew.hardin5+git@gmail.com>
2019-03-22 08:32:51 -0600

    Save collected files to disk; print keys as they're pressed.

--- spacecadet/src/matrix_collector.rs
+++ spacecadet/src/matrix_collector.rs
@@ -1,4 +1,6 @@
 use evdev_rs as evdev;
+use std::io::Write;
+use std::fs::File;
 
 fn main() {
     let instructions =
@@ -17,33 +19,87 @@ fn main() {
         \tTwo taps -> goto next row\n\
         \tThree taps -> finish\n";
     println!("{}", instructions);
+
+    // TODO: parse command line.
     let device_path = "/dev/input/event4";
+    let matrix_path = "/tmp/matrix.json";
+    let layer_path = "/tmp/layers.json";
 
     // Collect the matrix via user input.
     let matrix = collect_matrix(device_path);
-    let row_count = matrix.len();
     let col_count = matrix.iter().map(|x| x.len()).max().unwrap();
 
-    // Print the matrix.
-    for r in 0..row_count {
-        let loop_end = std::cmp::min(matrix[r].len(), col_count);
-        for j in 0..loop_end {
-            print!("{:?} ", matrix[r][j]);
-        }
-        for _j in loop_end..col_count {
-            print!("____");
-        }
-        println!();
-    }
-    /*
-    Two files:
-        Virtual Matrix: maps keys to locations.
-        Layers: maps locations to behavior.
 
-    We create both a virtual matrix and a layers file by default.
-    This provides a default behavior that can serve as a blank slate.
-    */
-    // FromStr() and ToStr()
+    // Create two documents:
+    //  Matrix = maps keys to matrix locations.
+    //  Layers = behavior of matrix locations.
+    //
+    // These documents could be created with a JSON library, but we'll
+    // create the documents using straight strings because we want the
+    // formatting to be perfect.
+
+    // Convert the every key code to a string.
+    let mut matrix : Vec<Vec<String>> = matrix.iter().map(|row| {
+        row.iter().map(|column| {
+            format!("\"{:?}\"", column).replacen("KEY", "KC", 1)
+        }).collect()
+    }).collect();
+
+    // Find the maximum width for each column.
+    let mut character_widths = vec![0; col_count];
+    for row in matrix.iter() {
+        for col in row.iter().enumerate() {
+            character_widths[col.0] = std::cmp::max(col.1.len(), character_widths[col.0]);
+        }
+    }
+
+    // Left pad each value to the max width of the column.
+    for row in matrix.iter_mut() {
+        for col in row.iter_mut().enumerate() {
+            *col.1 = format!("{:>width$}", col.1, width = character_widths[col.0])
+        }
+    }
+
+    // Some rows of the matrix may be missing keys (i.e. jagged).
+    // Fill in the missing keys.
+    for row in matrix.iter_mut() {
+        while row.len() < col_count {
+            let empty = "_".repeat(character_widths[row.len()] - 2); // don't include quotes.
+            row.push(format!("\"{}\"", empty));
+        }
+    }
+
+    // Combine strings.
+    // This is the literal JSON value that's written to the output JSON documents.
+    let left_pad_count = 4;
+    let matrix = matrix.iter().map(|x| {
+        // Pretty clear I'm not a rust expert...
+        " ".repeat(left_pad_count) + &"[ ".to_string() + &x.join(", ") + " ]"
+    }).collect::<Vec<String>>().join(",\n");
+
+    // This is ugly, but I want the output to have a very specific format.
+    let matrix_document = format!(r#"{{
+  "device": "{}",
+  "matrix": [
+{}
+  ]
+}}"#, device_path, matrix);
+
+    let layer_document = format!(r#"{{
+  "layer_order": [ "base" ],
+  "base": [
+{}
+  ]
+}}"#, matrix);
+
+    // Write the two output files.
+    write_to_file(matrix_path, &matrix_document);
+    write_to_file(layer_path, &layer_document);
+}
+
+fn write_to_file(path: &str, content: &str) {
+    let mut f = File::create(&path).unwrap();
+    f.write(content.as_bytes()).unwrap();
 }
 
 fn collect_matrix(name: &str) -> Vec<Vec<evdev::enums::EV_KEY>> {
@@ -82,9 +138,16 @@ fn collect_matrix(name: &str) -> Vec<Vec<evdev::enums::EV_KEY>> {
         }
 
         match last_key_press_count {
-            1 => matrix.last_mut().unwrap().push(last_key.clone().unwrap()),  // store
-            2 => matrix.push(Vec::new()),  // new row
-            3 => break(),  // finished
+            1 => {
+                matrix.last_mut().unwrap().push(last_key.clone().unwrap());
+                print!("{:?} ", last_key.clone().unwrap());
+                std::io::stdout().flush().unwrap();
+            },
+            2 => {
+                matrix.push(Vec::new());
+                println!();
+            },
+            3 => break(),
             _ => panic!()
         }
     }
